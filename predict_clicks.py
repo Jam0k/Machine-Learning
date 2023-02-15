8d2ccd260ef4feae9a3e2046e76cad32830a8b82
jam0k_ <james.mockford@outlook.com>
2023-02-15 12:50:32 +0000

    yes

--- predict_clicks.py
+++ predict_clicks.py
@@ -5,14 +5,14 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 
 # Read the data into a pandas DataFrame
-data = pd.read_csv("fb_ad_insight_api_transpose.csv")
+data = pd.read_csv("clickspend.csv")
 
 # Select the relevant columns
 data = data[["spend", "clicks"]]
 
 # Split the data into training and testing sets
 train_data = data[:int(len(data) * 0.8)]
-test_data = data[int(len(data) * 0.8):]
+test_data = data[int(len(data) * 0.2):]
 
 # Train the linear regression model on the training data
 X_train = train_data["spend"].values.reshape(-1, 1)
@@ -29,14 +29,6 @@ mse = mean_squared_error(y_test, y_pred)
 print("Mean Squared Error:", mse)
 
 # Use the trained model to predict the number of clicks for a new ad
-spend = 1000
+spend = float(input("Enter spend: "))
 predicted_clicks = reg.predict([[spend]])
 print("Predicted clicks:", predicted_clicks[0])
-
-# Plot the results
-plt.scatter(X_test, y_test, color='blue')
-plt.plot(X_test, y_pred, color='red')
-plt.xlabel("Spend")
-plt.ylabel("Clicks")
-plt.title("Estimated Clicks")
-plt.show()
