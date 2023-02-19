c31a50615a4179b7865b5133d6ebd3702a844601
jam0k_ <james.mockford@outlook.com>
2023-02-19 17:02:53 +0000

    y

--- create_sql_raw_table.py
+++ create_sql_raw_table.py
@@ -11,19 +11,20 @@ conn = psycopg2.connect(database="data", user="root", password="root", host="loc
 # Create a cursor
 cursor = conn.cursor()
 
-# Get the column names and data types from the CSV file
+# Get the column names and types from the CSV file
 columns = data.columns.tolist()
-column_types = [str(dtype) for dtype in data.dtypes.tolist()]
+column_types = ['text' if dtype == 'object' else 'numeric' for dtype in data.dtypes]
 
 # Create a table with the same column names and data types as the CSV file
-create_table_query = 'CREATE TABLE mytable (%s)'
-cursor.execute(create_table_query, [AsIs(','.join([f'{column} {column_type}' for column, column_type in zip(columns, column_types)]))])
+create_table_query = 'CREATE TABLE fb_ad_insight_api_transpose (%s)'
+column_names = [f'"{column}"' for column in columns]
+cursor.execute(create_table_query, [AsIs(','.join([f'{name} {data_type}' for name, data_type in zip(column_names, column_types)]))])
 
 # Insert the data into the table
 for _, row in data.iterrows():
     row_values = row.values.tolist()
     insert_query = 'INSERT INTO mytable (%s) VALUES %s'
-    cursor.execute(insert_query, (AsIs(','.join(columns)), tuple(row_values)))
+    cursor.execute(insert_query, (AsIs(','.join(column_names)), tuple(row_values)))
 
 # Commit the changes and close the connection
 conn.commit()
